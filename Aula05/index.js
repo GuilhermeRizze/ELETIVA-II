e0aef5b32c495973c5b4019cfec39236e7442689
GuilhermeRizze <guilhermerc20@gmail.com>
2023-03-21 20:41:35 -0300

    Aula 21/03

---
+++ Aula05/index.js
@@ -0,0 +1,48 @@
+//criando um localStorage
+var itens
+function carregar (){
+    itens = localStorage.getItem("AulaEletiva")
+    if (itens == null || itens == undefined){
+        var vetor =[
+            {
+                nome: "Guilherme",
+                idade: 20,
+                cores: ['verde', 'branco']
+            },
+            {
+                nome: "Josefina",
+                telefone: "1111-12-01",
+                cores: ['roxo', 'amarelo']
+            }
+        ]
+        itens = vetor
+        localStorage.setItem("AulaEletiva",JSON.stringify(vetor))
+        alert('criado')
+    }else{
+    itens = JSON.parse(itens)
+
+    itens.forEach( obj => {
+        var div = document.getElementById("nome")
+        div.append(obj.nome + "<br/>")
+    })
+    }
+}
+
+carregar()
+
+var salvar = () => {
+    var nome = document.getElementById("firstname").value
+    var idade = document.getElementById("idade").value
+
+    console.log(itens)
+
+    var save = {
+        nome, 
+        idade: idade
+    }
+
+    console.log("--------------------")
+
+    itens.push(save)
+    console.log(itens)
+}
